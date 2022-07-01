96c60a683ec869fe2679fa2a43d517911a3a6576
Mazen <mazenadel19@gmail.com>
2022-07-01 11:35:00 +0200

    HASURA_GRAPHQL_UNAUTHORIZED_ROLE 🙃

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { useQuery, gql } from "@apollo/client";
+
+const getTodos = gql`
+  query getTodos {
+    todos {
+      id
+      text
+      done
+    }
+  }
+`;
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(getTodos);
+
+  console.log({ loading, error, data });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.todos.map(({ id, text, done }) => (
+    <div key={id}>
+      <h3>{text}</h3>
+      <p>{done?'yes':'no'}</p>
+    </div>
+  ));
+}
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+      <DisplayLocations />
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const client = new ApolloClient({
+  uri: "https://liberal-ferret-71.hasura.app/v1/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+      <ApolloProvider client={client}>
+      <App />
+      </ApolloProvider>
   </React.StrictMode>
-)
+);
