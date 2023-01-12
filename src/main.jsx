edbf3c7c03084d90eb550584caf8a7726bf6c013
Mazen Emam <mazenadel19@gmail.com>
2023-01-13 00:52:12 +0200

    change url

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://graphqlchecklist.hasura.app/v1/graphql",
-    cache: new InMemoryCache(),
+  uri: "https://learninggraphqlz.hasura.app/v1/graphql",
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <ApolloProvider client={client}>
-            <App/>
-        </ApolloProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
 );
